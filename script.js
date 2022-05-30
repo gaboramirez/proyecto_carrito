14893ec9171cc350ed61a95a9a216ab61ff207f4
gaboramirez <ignacio98ramirez@gmail.com>
2022-05-30 20:50:23 -0300

    nuevas modificaciones

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const items = document.getElementById('items')
+const templateTarjeta = document.getElementById('template-tarjeta').content
+const fragment = document.createDocumentFragment()
+
 document.addEventListener('DOMContentLoad', () => {
     fetchData
 })
@@ -6,8 +10,18 @@ const fetchData = async () => {
     try {
         const res = await fetch('api.json')
         const data = await res.json()
-        console.log(data)
+        pintarTarjetas(data)
     } catch (error) {
         console.log(error)
     }
+}
+
+const pintarTarjetas = data => {
+    data.forEach(producto => {
+        templateTarjeta.querySelector('h5').textContent = producto.title
+
+        const doble = templateTarjeta.dobleNode(true)
+        fragment.appendChild(doble)
+    });
+    items.appendChild(fragment)
 }
